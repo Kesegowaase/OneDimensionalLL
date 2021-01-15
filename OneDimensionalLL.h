5f4aca91d78d585710ab58fdb3db2a60bbf6c4e1
Kesegowaase <61354054+Kesegowaase@users.noreply.github.com>
2021-01-15 22:35:40 +0300

    Update OneDimensionalLL.h

--- OneDimensionalLL.h
+++ OneDimensionalLL.h
@@ -172,10 +172,5 @@ void printAll(struct newNodeFB *startNewFB){
     
 }
 
-// You must write all the function definitions to be used in this lab into this file. 
-// You may also write other functions that may be called from our functions.
-// Do not make any changes to the main.c file.
-// Upload function.h file to the system as StudentNumber.h.
-
 
 
